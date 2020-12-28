220f4ab0277eafd55d6571e39f13b36f18ead5c9
andreferi31 <andreferi135@gmail.com>
2020-12-28 09:24:25 +0700

    Fix: Clean unecessary files

--- template.config.js
+++ template.config.js
@@ -1,11 +1,4 @@
 module.exports = {
-    // Placeholder used to rename and replace in files
-    // package.json, index.json, android/, ios/
     placeholderName: "ferisTemplate",
-
-    // Directory with template
     templateDir: "./ferisTemplate",
-
-    // Path to script, which will be executed after init
-    postInitScript: "./script.js"
 };
